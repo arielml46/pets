45954e75045fa2981f1a4accd201242f3419a754
ariel <arielmejiasleiton46@gmail.com>
2022-06-10 08:17:24 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "Top",
+        alignItems: "center",
+        font: "24rem",
+
+
+        
+        
+      }}
+
+      
+    >
+      <Text>Mantenimiento Mascotas</Text>
+
+      
+    </View>
+  );
+}
